c62b65dabbeb9ad9efe7d72f819d2c0f19a8d513
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-11-17 17:09:55 +0200

    refactor logic to use classes refactor test

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -8,34 +8,33 @@ describe("Calculator", () => {
     let currentYear = 2015;
 
     it('calculate working years left', () =>{
-      const calculator = new Calculator()
-      const yearsLeft = calculator.workingYearsLeft(currentAge, desiredRetirementAge)
+      const calculator = new Calculator(currentAge, desiredRetirementAge, currentYear)
+      const yearsLeft = calculator.workingYearsLeft()
       expect(yearsLeft).toEqual(40)
     });
     
    it('calculate the year of retirement', () =>{
-      const calculator = new Calculator()
-      const retirementYear = calculator.retirementYear(currentYear, calculator.workingYearsLeft(currentAge, desiredRetirementAge))
+      const calculator = new Calculator(currentAge, desiredRetirementAge, currentYear)
+      const retirementYear = calculator.retirementYear(calculator.workingYearsLeft(currentAge, desiredRetirementAge))
       expect(retirementYear).toEqual(2055)
     });
   })
 
-  describe('First scenerio', ()=>{
+  describe('second scenerio', ()=>{
     let currentAge = 15;
     let desiredRetirementAge = 65;
     let currentYear = 2010;
 
     it('calculate working years left', () =>{
-      const calculator = new Calculator()
-      const yearsLeft = calculator.workingYearsLeft(currentAge, desiredRetirementAge)
+      const calculator = new Calculator(currentAge, desiredRetirementAge, currentYear)
+      const yearsLeft = calculator.workingYearsLeft()
       expect(yearsLeft).toEqual(50)
     });
     
    it('calculate the year of retirement', () =>{
-      const calculator = new Calculator()
-      const retirementYear = calculator.retirementYear(currentYear, calculator.workingYearsLeft(currentAge, desiredRetirementAge))
+      const calculator = new Calculator(currentAge, desiredRetirementAge, currentYear)
+      const retirementYear = calculator.retirementYear(calculator.workingYearsLeft(currentAge, desiredRetirementAge))
       expect(retirementYear).toEqual(2060)
     });
   })
-  
 })
