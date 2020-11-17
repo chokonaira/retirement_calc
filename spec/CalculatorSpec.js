d2f50b694d003a1ccb912ac20004c13f4842c1b8
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-11-17 12:45:19 +0200

    Adds test to calculate retirement year Adds code logic to make it pass Add README file

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -8,5 +8,13 @@ describe("Calculator", () => {
     let desiredRetirementAge = 65;
     const yearsLeft = calculator.workingYearsLeft(currentAge, desiredRetirementAge)
     expect(yearsLeft).toEqual(40)
-  })
+  });
+  
+ it('calculate the year of retirement', () =>{
+    calculator = new Calculator()
+    let currentYear = 2015;
+    let yearsLeft = 40;
+    const retirementYear = calculator.retirementYear(currentYear, yearsLeft)
+    expect(retirementYear).toEqual(2055)
+  });
 })
