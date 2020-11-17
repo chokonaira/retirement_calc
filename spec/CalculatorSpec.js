4684ca9a13bd3f0a4b3a7887c5f5c78e6926d335
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-11-17 13:09:27 +0200

    Adds test to check for another retirement scenerio Made code logic dynamic

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,20 +1,40 @@
 describe("Calculator", () => {
   const Calculator = require('../lib/Calculator');
 
-
-  it('calculate working years left', () =>{
-    calculator = new Calculator()
+  describe('First scenerio', ()=>{
     let currentAge = 25;
     let desiredRetirementAge = 65;
-    const yearsLeft = calculator.workingYearsLeft(currentAge, desiredRetirementAge)
-    expect(yearsLeft).toEqual(40)
-  });
-  
- it('calculate the year of retirement', () =>{
-    calculator = new Calculator()
     let currentYear = 2015;
-    let yearsLeft = 40;
-    const retirementYear = calculator.retirementYear(currentYear, yearsLeft)
-    expect(retirementYear).toEqual(2055)
-  });
+
+    it('calculate working years left', () =>{
+      calculator = new Calculator()
+      const yearsLeft = calculator.workingYearsLeft(currentAge, desiredRetirementAge)
+      expect(yearsLeft).toEqual(40)
+    });
+    
+   it('calculate the year of retirement', () =>{
+      calculator = new Calculator()
+      const retirementYear = calculator.retirementYear(currentYear, calculator.workingYearsLeft(currentAge, desiredRetirementAge))
+      expect(retirementYear).toEqual(2055)
+    });
+  })
+
+  describe('First scenerio', ()=>{
+    let currentAge = 15;
+    let desiredRetirementAge = 65;
+    let currentYear = 2010;
+
+    it('calculate working years left', () =>{
+      calculator = new Calculator()
+      const yearsLeft = calculator.workingYearsLeft(currentAge, desiredRetirementAge)
+      expect(yearsLeft).toEqual(50)
+    });
+    
+   it('calculate the year of retirement', () =>{
+      calculator = new Calculator()
+      const retirementYear = calculator.retirementYear(currentYear, calculator.workingYearsLeft(currentAge, desiredRetirementAge))
+      expect(retirementYear).toEqual(2060)
+    });
+  })
+  
 })
